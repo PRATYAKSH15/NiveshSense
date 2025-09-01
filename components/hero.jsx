c6fe91b399c6dda54a9a173fd85fd89144a284ec
Pratyaksh <pratyaksh1594@gmail.com>
2025-09-01 18:38:06 +0530

    auth setup

---
+++ components/hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HeroSection() {
+  return (
+    <div>
+        <section className="bg-white dark:bg-gray-900">this is my hero section</section>
+    </div>
+  )
+}
+
+export default HeroSection
